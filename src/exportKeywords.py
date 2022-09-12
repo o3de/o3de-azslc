c632412c17b1ec9c013b0f9ae9fa4db79e241379
Vivien Oddou <vivien.oddou@siliconstudio.co.jp>
2022-09-12 09:07:38 +0900

    Updated copyright comment in export keyword script
    Signed-off-by: Vivien Oddou <vivien.oddou@siliconstudio.co.jp>

--- src/exportKeywords.py
+++ src/exportKeywords.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
-its licensors.
+Copyright (c) Contributors to the Open 3D Engine Project.
+For complete copyright and license terms please see the LICENSE at the root of this distribution.
 
-For complete copyright and license terms please see the LICENSE at the root of this
-distribution (the "License"). All use of this software is governed by the License,
-or, if provided, by the license below or the license accompanying this file. Do not
-remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 import sys
 import os
@@ -40,14 +36,10 @@ if __name__ == "__main__":
 
     with open("AzslcPredefinedTypes.h", 'w') as outfile:
         copyright = '''/*
-* All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
-* its licensors.
+* Copyright (c) Contributors to the Open 3D Engine Project.
+* For complete copyright and license terms please see the LICENSE at the root of this distribution.
 *
-* For complete copyright and license terms please see the LICENSE at the root of this
-* distribution (the "License"). All use of this software is governed by the License,
-* or, if provided, by the license below or the license accompanying this file. Do not
-* remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* SPDX-License-Identifier: Apache-2.0 OR MIT
 *
 */
 #pragma once
