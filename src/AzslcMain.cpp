2735fae87eea78a57fcbd2011dd70a3b7a8573b3
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-08-25 19:10:35 +0200

    Fix CLI::BadNameString
    Signed-off-by: Guilhem Saurel <guilhem.saurel@laas.fr>

--- src/AzslcMain.cpp
+++ src/AzslcMain.cpp
@@ -321,7 +321,7 @@ int main(int argc, const char* argv[])
     cli.add_flag("--version", printVersion, "Prints version information");
 
     std::string inputFile;
-    cli.add_option("(- | FILE)", inputFile, "Input file (pass - to read from stdin).");
+    cli.add_option("FILE", inputFile, "Input file (pass - to read from stdin).");
 
     std::string output;
     cli.add_option("-o", output, "Output file (writes to stdout if omitted).");
